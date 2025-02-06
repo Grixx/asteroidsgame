c08c80aab14102a4c52516414234a84109c353db
Grixx <jbullardgit@fastmail.com>
2025-02-06 02:26:35 -0500

    astroids break into smaller bits when shot now

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
-
+from constants import ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -12,3 +13,14 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += (self.velocity * dt)
         
+    def split(self):
+        random_angle = random.uniform(20, 50)
+        speed = random._inst
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            new1 = Asteroid(self.position.x, self.position.y, (self.radius - ASTEROID_MIN_RADIUS))
+            new2 = Asteroid(self.position.x, self.position.y, (self.radius - ASTEROID_MIN_RADIUS))
+            new1.velocity = self.velocity.rotate(-random_angle) * 1.2
+            new2.velocity = self.velocity.rotate(random_angle) * 1.2
